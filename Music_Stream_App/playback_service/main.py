c31ef8ee1a936cb773ff10d780168ab88a1da96e
RinAdriann <149231126+RinAdriann@users.noreply.github.com>
2025-06-07 09:23:16 +0000

    made everything runable from github codespaces

--- Music_Stream_App/playback_service/main.py
+++ Music_Stream_App/playback_service/main.py
@@ -3,3 +3,7 @@ from playback_service.routes import router
 
 app = FastAPI(title="Playback Service")
 app.include_router(router)
+
+@app.get("/")
+async def root():
+    return {"message": "Playback Service is running!"}
